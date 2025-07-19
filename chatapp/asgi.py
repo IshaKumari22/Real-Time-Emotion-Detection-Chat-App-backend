42c1b8f39731ae95759b1aa0d7598fe39e42f88b
Isha Kumari <isha02740@gmail.com>
2025-07-19 21:53:13 +0530

    ready to start websocket

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -14,3 +14,14 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 
 application = get_asgi_application()
+
+
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# import chat.routing
+
+# application = ProtocolTypeRouter({
+#     'websocket': AuthMiddlewareStack(
+#         URLRouter(chat.routing.websocket_urlpatterns)
+#     ),
+# })
